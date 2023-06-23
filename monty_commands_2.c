04c36c25ac1d8d0a3ba6515f65b77db749661655
Tituz175 <tobititus01@gmail.com>
2023-06-22 20:00:56 -0600

    monty task div modified inline with the requirement

--- monty_commands_2.c
+++ monty_commands_2.c
@@ -101,11 +101,23 @@ void monty_div(stack_t **head, unsigned int counter)
 	}
 	else
 	{
-		temp = current->next;
-		value1 = current->n;
-		value2 = temp->n;
-		temp->n = value2 / value1;
-		*head = temp;
-		free(current);
+		if (current->n == 0)
+		{
+			fprintf(stderr, "L%d: division by zero\n", counter);
+			fclose(file_parameters.file);
+			free(file_parameters.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE);
+
+		}
+		else
+		{
+			temp = current->next;
+			value1 = current->n;
+			value2 = temp->n;
+			temp->n = value2 / value1;
+			*head = temp;
+			free(current);
+		}
 	}
 }
