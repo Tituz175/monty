43e0c2c6ba631223dc1bc3efb973feb6bc788a0b
Tituz175 <tobititus01@gmail.com>
2023-06-22 20:18:31 -0600

    monty mod task modified again inline with the requirement

--- monty_commands_3.c
+++ monty_commands_3.c
@@ -37,7 +37,7 @@ void monty_mod(stack_t **head, unsigned int counter)
 			temp = current->next;
 			value1 = current->n;
 			value2 = temp->n;
-			temp->n = value1 % value2;
+			temp->n = value2 % value1;
 			*head = temp;
 			free(current);
 		}
